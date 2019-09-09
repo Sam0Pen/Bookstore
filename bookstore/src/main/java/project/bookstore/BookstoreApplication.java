1fbe5b617d2f1ae31a7e08d275cce2e5cec69c11
Sami Jaanu <jaanusa.sj@gmail.com>
2019-09-09 11:31:17 +0300

    päivityksiä

--- bookstore/src/main/java/project/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/project/bookstore/BookstoreApplication.java
@@ -1,13 +1,35 @@
 package project.bookstore;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+
+import project.bookstore.domain.Book;
+import project.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository bookRepository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			bookRepository.save(new Book("Lord of the rings", "J.R.R Tolkien", 1954, "9788373191723", 24));
+		
+			log.info("fetch all books");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
+		};
 	}
 
 }
--- bookstore/src/main/java/project/bookstore/controller/BookController.java
+++ bookstore/src/main/java/project/bookstore/controller/BookController.java
@@ -2,15 +2,20 @@ package project.bookstore.controller;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import project.bookstore.domain.Book;
+import project.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+	
+	@Autowired
+	BookRepository bookRepository;
 	
 	@RequestMapping(value= "/index", method = RequestMethod.GET)
 	public String newBook(Model model){
